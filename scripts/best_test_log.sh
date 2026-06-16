ab66545b9c0cae66d3d3d1456945d08cc1314822
xunjijiang <xun_ji@foxmail.com>
2026-06-16 16:11:41 +0800

    feat: 修改输出

--- scripts/best_test_log.sh
+++ scripts/best_test_log.sh
@@ -24,6 +24,31 @@ if [[ -z "$LOG_PATH" ]]; then
   exit 1
 fi
 
+# 低于 92 的数量
+LOWER_THAN_92_COUNT=0
+# 92 - 92.1 的数量
+BETWEEN_92_AND_921_COUNT=0
+# 92.1 - 92.2 的数量
+BETWEEN_921_AND_922_COUNT=0
+# 92.2 - 92.3 的数量
+BETWEEN_922_AND_923_COUNT=0
+# 92.3 - 92.4 的数量
+BETWEEN_923_AND_924_COUNT=0
+# 92.4 - 92.5 的数量
+BETWEEN_924_AND_925_COUNT=0
+# 92.5 - 92.6 的数量
+BETWEEN_925_AND_926_COUNT=0
+# 92.6 - 92.7 的数量
+BETWEEN_926_AND_927_COUNT=0
+# 92.7 - 92.8 的数量
+BETWEEN_927_AND_928_COUNT=0
+# 92.8 - 92.9 的数量
+BETWEEN_928_AND_929_COUNT=0
+# 92.9 - 93 的数量
+BETWEEN_929_AND_930_COUNT=0
+# 高于 93 的数量
+HIGHER_THAN_93_COUNT=0
+
 # 读取日志文件，提取测试结果并找到最佳结果
 BEST_RESULT=""
 
@@ -50,6 +75,34 @@ for file in ${LOG_PATH}/log-*.csv; do
 
     echo "File: $file, Mean Score: $MEAN_SCORE"
 
+    if (( $(echo "$MEAN_SCORE < 92" | bc -l) )); then
+      ((LOWER_THAN_92_COUNT++))
+    elif (( $(echo "$MEAN_SCORE >= 92 && $MEAN_SCORE < 92.1" | bc -l) )); then
+      ((BETWEEN_92_AND_921_COUNT++))
+    elif (( $(echo "$MEAN_SCORE >= 92.1 && $MEAN_SCORE < 92.2" | bc -l) )); then
+      ((BETWEEN_921_AND_922_COUNT++))
+    elif (( $(echo "$MEAN_SCORE >= 92.2 && $MEAN_SCORE < 92.3" | bc -l) )); then
+      ((BETWEEN_922_AND_923_COUNT++))
+    elif (( $(echo "$MEAN_SCORE >= 92.3 && $MEAN_SCORE < 92.4" | bc -l) )); then
+      ((BETWEEN_923_AND_924_COUNT++))
+    elif (( $(echo "$MEAN_SCORE >= 92.4 && $MEAN_SCORE < 92.5" | bc -l) )); then
+      ((BETWEEN_924_AND_925_COUNT++))
+    elif (( $(echo "$MEAN_SCORE >= 92.5 && $MEAN_SCORE < 92.6" | bc -l) )); then
+      ((BETWEEN_925_AND_926_COUNT++))
+    elif (( $(echo "$MEAN_SCORE >= 92.6 && $MEAN_SCORE < 92.7" | bc -l) )); then
+      ((BETWEEN_926_AND_927_COUNT++))
+    elif (( $(echo "$MEAN_SCORE >= 92.7 && $MEAN_SCORE < 92.8" | bc -l) )); then
+      ((BETWEEN_927_AND_928_COUNT++))
+    elif (( $(echo "$MEAN_SCORE >= 92.8 && $MEAN_SCORE < 92.9" | bc -l) )); then
+      ((BETWEEN_928_AND_929_COUNT++))
+    elif (( $(echo "$MEAN_SCORE >= 92.9 && $MEAN_SCORE < 93" | bc -l) )); then
+      ((BETWEEN_929_AND_930_COUNT++))
+    elif (( $(echo "$MEAN_SCORE >= 92.8 && $MEAN_SCORE < 0" | bc -l) )); then
+      echo "Error: Invalid score"
+    else
+      ((HIGHER_THAN_93_COUNT++))
+    fi
+
     if [[ -z "$BEST_RESULT" || $(echo "$MEAN_SCORE > $BEST_RESULT" | bc) -eq 1 ]]; then
       BEST_RESULT="$MEAN_SCORE"
       BEST_FILE="$file"
@@ -61,6 +114,18 @@ IFS="$OLD_IFS"  #恢复shell默认分割符配置
 
 
 if [[ -n "$BEST_FILE" ]]; then
+  echo "< 92.0      : $LOWER_THAN_92_COUNT"
+  echo "92.0 =~ 92.1: $BETWEEN_92_AND_921_COUNT"
+  echo "92.1 =~ 92.2: $BETWEEN_921_AND_922_COUNT"
+  echo "92.2 =~ 92.3: $BETWEEN_922_AND_923_COUNT"
+  echo "92.3 =~ 92.4: $BETWEEN_923_AND_924_COUNT"
+  echo "92.4 =~ 92.5: $BETWEEN_924_AND_925_COUNT"
+  echo "92.5 =~ 92.6: $BETWEEN_925_AND_926_COUNT"
+  echo "92.6 =~ 92.7: $BETWEEN_926_AND_927_COUNT"
+  echo "92.7 =~ 92.8: $BETWEEN_927_AND_928_COUNT"
+  echo "92.8 =~ 92.9: $BETWEEN_928_AND_929_COUNT"
+  echo "92.9 =~ 93.0: $BETWEEN_929_AND_930_COUNT"
+  echo "> 93.0      : $HIGHER_THAN_93_COUNT"
   echo "Best result found in file: $BEST_FILE with mean score: $BEST_RESULT"
 else
   echo "No log files found in $LOG_PATH"
