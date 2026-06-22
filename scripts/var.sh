b2e99a30847ae6dd0a9dd3095c0e921480fe1c9f
xunjijiang <xun_ji@foxmail.com>
2026-06-22 18:22:09 +0800

    feat: 修改数据存储路径

--- scripts/var.sh
+++ scripts/var.sh
@@ -7,9 +7,9 @@ source "${PROJECT_DIR}/.venv/bin/activate"
 MODEL_TYPE="mrad-clip"
 
 # Path configuration
-CHECKPOINT_DIR="${PROJECT_DIR}/checkpoints/released_${MODEL_TYPE}"
-LOG_DIR="${PROJECT_DIR}/logs/released_${MODEL_TYPE}"
-CACHE_DIR="${PROJECT_DIR}/cache"
+CHECKPOINT_DIR="${PROJECT_DIR}/checkpoints/released_${MODEL_TYPE}-multi-scale-memory"
+LOG_DIR="${PROJECT_DIR}/logs/released_${MODEL_TYPE}-multi-scale-memory"
+CACHE_DIR="${PROJECT_DIR}/cache-multi-scale-memory"
 TRAIN_SCRIPT="${PROJECT_DIR}/train.py"
 TEST_SCRIPT="${PROJECT_DIR}/test.py"
 
